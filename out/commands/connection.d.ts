02f19485e8230fd361070420c9cc6025eb515bab
tctinh <tctinh@tma.com.vn>
2026-01-03 17:24:22 +0700

    Add .gitignore

--- out/commands/connection.d.ts
+++
@@ -1,5 +0,0 @@
-import * as vscode from 'vscode';
-import { ConnectionManager } from '../core/ConnectionManager';
-import { LangfuseTreeProvider } from '../views/TreeProvider';
-export declare function registerConnectionCommands(context: vscode.ExtensionContext, connectionManager: ConnectionManager, treeProvider: LangfuseTreeProvider): void;
-//# sourceMappingURL=connection.d.ts.map
--- out/commands/trace.d.ts
+++
@@ -1,4 +0,0 @@
-import * as vscode from 'vscode';
-import { ConnectionManager } from '../core/ConnectionManager';
-export declare function registerTraceCommands(context: vscode.ExtensionContext, connectionManager: ConnectionManager): void;
-//# sourceMappingURL=trace.d.ts.map
